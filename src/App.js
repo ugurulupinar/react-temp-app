82d61df770e436a47e158908a1da13a43a27adef
UĞUR <ugur.ulupinar2102@gmail.com>
2023-11-19 16:55:34 +0300

    git setting and env var

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import logo from "./logo.svg"
 function App() {
   return (
     <div className="App">
+      
+      <h3>{process.env.NODE_ENV}</h3>
       <img src="/logo192.png" alt="" />
       <img src={logo} alt="" />
 
